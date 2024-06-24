3afedda157d86b9ebea67d173f64c0bc95356912
Daniel Porras Morales <danielpm.dev@gmail.com>
2024-06-24 10:24:09 +0200

    Añado el proyecto con las funcionalidades base

---
+++ src/components/Search.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Search(){
+    const [value, setValue] = useState('')
+    const buscar = (e) => setValue(e.target.value)
+    const url = "results/"
+
+    return(
+        <div>
+            <center>
+                <h2 class="mt-5">Search Movie</h2>
+                <form action={url + value} method="POST">
+                    <input type="search" class="form-control p-3" value={value} onChange={buscar} placeholder="Search" />
+                </form>
+            </center>
+        </div>
+    )
+}
